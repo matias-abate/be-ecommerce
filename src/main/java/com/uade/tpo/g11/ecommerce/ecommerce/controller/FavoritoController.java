9e43eddcf3ca6ffe595d95678ede0d88c897390e
Matias Abate <matiasalessandro1999@gmail.com>
2024-08-21 00:12:15 -0300

    Vinculado desde intellij

--- src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/controller/FavoritoController.java
+++ src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/controller/FavoritoController.java
@@ -1,6 +1,5 @@
 package com.uade.tpo.g11.ecommerce.ecommerce.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.uade.tpo.g11.ecommerce.ecommerce.entity.Favorito;
--- src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/entity/Carrito.java
+++ src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/entity/Carrito.java
@@ -1,4 +1,5 @@
 package com.uade.tpo.g11.ecommerce.ecommerce.entity;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,26 +8,20 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-
 @Entity
 @Table(name = "carrito")
 public class Carrito {
+
     @Id
-    @GeneratedValue(
-            strategy = GenerationType.IDENTITY
-    )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @ManyToOne
-    @JoinColumn(
-            name = "usuarioId",
-            nullable = false
-    )
+    @JoinColumn(name = "usuarioId", nullable = false)
     private Usuario usuario;
+
     @ManyToOne
-    @JoinColumn(
-            name = "productoId",
-            nullable = false
-    )
+    @JoinColumn(name = "productoId", nullable = false)
     private Producto producto;
 
     public Carrito() {
