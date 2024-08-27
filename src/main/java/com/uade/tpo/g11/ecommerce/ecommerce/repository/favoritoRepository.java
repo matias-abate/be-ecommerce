a93af166738e536269d08b5a356a39d4552dca64
LucasR <lrubini@uade.edu.ar>
2024-08-27 20:19:57 -0300

    Favorito Update

--- src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/repository/favoritoRepository.java
+++ src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/repository/favoritoRepository.java
@@ -1,7 +1,13 @@
 package com.uade.tpo.g11.ecommerce.ecommerce.repository;
 
 import com.uade.tpo.g11.ecommerce.ecommerce.entity.Favorito;
+import com.uade.tpo.g11.ecommerce.ecommerce.entity.Producto;
+import com.uade.tpo.g11.ecommerce.ecommerce.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface favoritoRepository extends JpaRepository<Favorito, Long> {
+import java.util.List;
+
+public interface FavoritoRepository extends JpaRepository<Favorito, Integer> {
+    List<Favorito> findByUsuario(Usuario usuario);
+    void deleteByUsuarioAndProducto(Usuario usuario, Producto producto);
 }
