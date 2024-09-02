d7fa4dba5ab8bbf31d35dad969bcfe404d083b42
lucasvegalv <lucasvega02@gmail.com>
2024-09-02 18:53:33 -0300

    feature: Implement Product Service and Controller

---
+++ src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/controllers/ProductController.java
@@ -0,0 +1,53 @@
+package com.uade.tpo.g11.ecommerce.ecommerce.controllers;
+
+import com.uade.tpo.g11.ecommerce.ecommerce.dtos.ProductDTO;
+import com.uade.tpo.g11.ecommerce.ecommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    // GET ALL
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> getAllProducts() {
+        List<ProductDTO> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    // GET BY ID
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable int id) {
+        ProductDTO product = productService.getProductById(id);
+        return ResponseEntity.ok(product);
+    }
+
+    // POST
+    @PostMapping
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO product) {
+        ProductDTO newProduct = productService.createProduct(product);
+        return ResponseEntity.ok(newProduct);
+    }
+
+    // PUT
+    @PutMapping
+    public ResponseEntity<ProductDTO> updateProduct(@RequestBody ProductDTO product) {
+        ProductDTO updatedProduct = productService.updateProduct(product);
+        return ResponseEntity.ok(updatedProduct);
+    }
+
+    // DELETE
+    @DeleteMapping
+    public ResponseEntity<Void> deleteProduct(@RequestBody ProductDTO product) {
+        productService.deleteProduct(product.getId());
+        return ResponseEntity.noContent().build();
+    }
+
+}
