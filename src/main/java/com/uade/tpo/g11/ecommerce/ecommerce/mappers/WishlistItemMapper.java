6fad5d67c5b6afb316e810da4a9c0179b62f993b
lucasvegalv <lucasvega02@gmail.com>
2024-09-04 12:59:24 -0300

    feature: Implement Wishlist and WishlistItem mappers

--- src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/mappers/WishlistItemMapper.java
+++ src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/mappers/WishlistItemMapper.java
@@ -1,4 +1,60 @@
 package com.uade.tpo.g11.ecommerce.ecommerce.mappers;
 
+import com.uade.tpo.g11.ecommerce.ecommerce.dtos.WishlistItemDTO;
+import com.uade.tpo.g11.ecommerce.ecommerce.entities.ProductEntity;
+import com.uade.tpo.g11.ecommerce.ecommerce.entities.WishlistEntity;
+import com.uade.tpo.g11.ecommerce.ecommerce.entities.WishlistItemEntity;
+import com.uade.tpo.g11.ecommerce.ecommerce.repositories.IProductRepository;
+import com.uade.tpo.g11.ecommerce.ecommerce.repositories.IWishlistItemRepository;
+import com.uade.tpo.g11.ecommerce.ecommerce.repositories.IWishlistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class WishlistItemMapper {
+
+    @Autowired
+    IWishlistRepository wishlistRepository;
+
+    @Autowired
+    WishlistMapper wishlistMapper;
+
+    @Autowired
+    IProductRepository productRepository;
+
+    @Autowired
+    ProductMapper productMapper;
+
+    public WishlistItemDTO toDTO(WishlistItemEntity wishlistItemEntity) {
+        WishlistItemDTO wishlistItemDTO = new WishlistItemDTO();
+
+        // ID Setter
+        wishlistItemDTO.setWishlistItemId(wishlistItemEntity.getWishlistItemId());
+
+        // Wishlist Setter
+        WishlistEntity wishlistEntity = wishlistRepository.findById(wishlistItemEntity.getWishlistItemId()).orElse(null);
+        wishlistItemDTO.setWishlist(wishlistMapper.toDTO(wishlistEntity));
+
+        // Product Setter
+        ProductEntity productEntity = productRepository.findById(wishlistItemEntity.getProduct().getProductId()).orElse(null);
+        wishlistItemDTO.setProduct(productMapper.toDTO(productEntity));
+
+        return wishlistItemDTO;
+    }
+
+    public WishlistItemEntity toEntity(WishlistItemDTO wishlistItemDTO) {
+        WishlistItemEntity wishlistItemEntity = new WishlistItemEntity();
+
+        // ID Setter
+        wishlistItemEntity.setWishlistItemId(wishlistItemDTO.getWishlistItemId());
+
+        // Wishlist Setter
+        wishlistItemEntity.setWishlist(wishlistMapper.toEntity(wishlistItemDTO.getWishlist()));
+
+        // Product Setter
+        wishlistItemEntity.setProduct(productMapper.toEntity(wishlistItemDTO.getProduct()));
+
+        return wishlistItemEntity;
+    }
+
 }
--- src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/mappers/WishlistMapper.java
+++ src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/mappers/WishlistMapper.java
@@ -2,11 +2,18 @@ package com.uade.tpo.g11.ecommerce.ecommerce.mappers;
 
 import com.uade.tpo.g11.ecommerce.ecommerce.dtos.UserDTO;
 import com.uade.tpo.g11.ecommerce.ecommerce.dtos.WishlistDTO;
+import com.uade.tpo.g11.ecommerce.ecommerce.dtos.WishlistItemDTO;
 import com.uade.tpo.g11.ecommerce.ecommerce.entities.UserEntity;
 import com.uade.tpo.g11.ecommerce.ecommerce.entities.WishlistEntity;
+import com.uade.tpo.g11.ecommerce.ecommerce.entities.WishlistItemEntity;
 import com.uade.tpo.g11.ecommerce.ecommerce.repositories.IUserRepository;
+import com.uade.tpo.g11.ecommerce.ecommerce.repositories.IWishlistItemRepository;
+import com.uade.tpo.g11.ecommerce.ecommerce.repositories.IWishlistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class WishlistMapper {
@@ -16,6 +23,20 @@ public class WishlistMapper {
 
     @Autowired
     UserMapper userMapper;
+
+    @Autowired
+    IWishlistItemRepository wishlistItemRepository;
+
+    @Autowired
+    WishlistItemMapper wishlistItemMapper;
+
+
+    @Autowired
+    IWishlistRepository wishlistRepository;
+
+    @Autowired
+    WishlistMapper wishlistMapper;
+
 
     public WishlistDTO toDTO(WishlistEntity wishlistEntity) {
 
@@ -31,13 +52,39 @@ public class WishlistMapper {
         }
 
         // WishlistItem List Setter
-        //wishlistDTO.setWishlistItems();
+        // Filtramos los items que correspondan a nuestra wishlist
+        List<WishlistItemEntity> wishlistItems = wishlistItemRepository.findAll().stream()
+                .filter(wishlistItem -> wishlistItem.getWishlist().getWishlistId() == wishlistEntity.getWishlistId())
+                .collect(Collectors.toList());
+
+        // Convertimos esos items previamente filtrados a DTOs para poder settearlos
+        List<WishlistItemDTO> wishlistItemDTOS = wishlistItems.stream()
+                .map(wishlistItemMapper::toDTO)
+                .collect(Collectors.toList());
+
+        wishlistDTO.setWishlistItems(wishlistItemDTOS);
 
         return wishlistDTO;
     }
 
     public WishlistEntity toEntity(WishlistDTO wishlistDTO) {
 
+        WishlistEntity wishlistEntity = new WishlistEntity();
+
+        // ID Setter
+        wishlistEntity.setWishlistId(wishlistDTO.getWishlistId());
+
+        // User Setter
+        UserEntity user = userMapper.toEntity(wishlistDTO.getUser());
+
+        // WishlistItem List Setter
+        List<WishlistItemEntity> wishlistItems = wishlistDTO.getWishlistItems().stream()
+                .map(wishlistItemMapper::toEntity)
+                .collect(Collectors.toList());
+
+        wishlistEntity.setWishlistItems(wishlistItems);
+
+        return wishlistEntity;
     }
 
 }
