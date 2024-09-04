2a911061869ee682760417df2c842c676a4a40c8
lucasvegalv <lucasvega02@gmail.com>
2024-09-04 12:30:51 -0300

    feature: Implement Wishlist and WishlistItem repositories

--- src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/mappers/WishlistMapper.java
+++ src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/mappers/WishlistMapper.java
@@ -1,7 +1,43 @@
 package com.uade.tpo.g11.ecommerce.ecommerce.mappers;
 
+import com.uade.tpo.g11.ecommerce.ecommerce.dtos.UserDTO;
+import com.uade.tpo.g11.ecommerce.ecommerce.dtos.WishlistDTO;
+import com.uade.tpo.g11.ecommerce.ecommerce.entities.UserEntity;
+import com.uade.tpo.g11.ecommerce.ecommerce.entities.WishlistEntity;
+import com.uade.tpo.g11.ecommerce.ecommerce.repositories.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class WishlistMapper {
 
-    public
+    @Autowired
+    IUserRepository userRepository;
+
+    @Autowired
+    UserMapper userMapper;
+
+    public WishlistDTO toDTO(WishlistEntity wishlistEntity) {
+
+        WishlistDTO wishlistDTO = new WishlistDTO();
+
+        // ID Setter
+        wishlistDTO.setWishlistId(wishlistEntity.getWishlistId());
+
+        // User Setter
+        UserEntity user = userRepository.findById(wishlistEntity.getUser().getUserId()).orElse(null);
+        if(user != null) {
+            wishlistDTO.setUser(userMapper.toDTO(user));
+        }
+
+        // WishlistItem List Setter
+        //wishlistDTO.setWishlistItems();
+
+        return wishlistDTO;
+    }
+
+    public WishlistEntity toEntity(WishlistDTO wishlistDTO) {
+
+    }
 
 }
---
+++ src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/repositories/IWishlistRepository.java
@@ -0,0 +1,9 @@
+package com.uade.tpo.g11.ecommerce.ecommerce.repositories;
+
+import com.uade.tpo.g11.ecommerce.ecommerce.entities.WishlistEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IWishlistRepository extends JpaRepository<WishlistEntity, Integer> {
+}
