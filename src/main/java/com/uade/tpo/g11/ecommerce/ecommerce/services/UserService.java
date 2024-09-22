561f1347c9c14ccd5b4347441f5728aba10ac462
shir011 <108352884+shir011@users.noreply.github.com>
2024-09-22 18:36:01 -0300

    update user

--- src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/services/UserService.java
+++ src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/services/UserService.java
@@ -1,14 +1,11 @@
 package com.uade.tpo.g11.ecommerce.ecommerce.services;
 
-import com.uade.tpo.g11.ecommerce.ecommerce.dtos.OrderDTO;
 import com.uade.tpo.g11.ecommerce.ecommerce.dtos.UserDTO;
-import com.uade.tpo.g11.ecommerce.ecommerce.entities.TransactionEntity;
 import com.uade.tpo.g11.ecommerce.ecommerce.entities.UserEntity;
-import com.uade.tpo.g11.ecommerce.ecommerce.mappers.OrderMapper;
 import com.uade.tpo.g11.ecommerce.ecommerce.mappers.UserMapper;
-import com.uade.tpo.g11.ecommerce.ecommerce.repositories.IOrderRepository;
 import com.uade.tpo.g11.ecommerce.ecommerce.repositories.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,13 +17,9 @@ public class UserService {
 
     @Autowired
     IUserRepository userRepository;
-    @Autowired
-    IOrderRepository orderRepository;
 
     @Autowired
     UserMapper userMapper;
-    @Autowired
-    private OrderMapper orderMapper;
 
     @Autowired
     public UserService(IUserRepository userRepository) {
@@ -57,49 +50,53 @@ public class UserService {
     }
 
     // CREATE
-    public void createUser(UserDTO userDTO) {
-        UserEntity userEntity = userMapper.toEntity(userDTO);
-        userRepository.save(userEntity);
+    public UserDTO createUser(UserDTO userDTO) {
+        try {
+            UserEntity userEntity = userMapper.toEntity(userDTO);
+            UserEntity savedUser = userRepository.save(userEntity);
+            return userMapper.toDTO(savedUser);
+        } catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("Username: '" + userDTO.getUsername() + "' ya existe");
+        }
     }
+
 
 
     // UPDATE
-    public UserDTO updateUser(Integer id, UserDTO userDTO) {
-        Optional<UserEntity> existingUser = userRepository.findById(id);
-        UserDTO updatedUser = null;
-
-        if(existingUser.isPresent()) {
-            UserEntity userEntity = existingUser.get();
-
-            userEntity.setUsername(userDTO.getUsername());
-            userEntity.setEmail(userDTO.getEmail());
-            userEntity.setBirthDate(userDTO.getBirth());
-            userEntity.setRole(userDTO.getRole());
-            userEntity.setFirstname(userDTO.getFirstname());
-            userEntity.setLastname(userDTO.getLastname());
-
-            UserEntity user = userRepository.save(userEntity);
-            updatedUser = userMapper.toDTO(user);
-        }
-
-        return updatedUser;
+    public UserDTO updateUser(int id, UserDTO userDTO) {
+        UserEntity existingUser = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        userMapper.updateEntityFromDTO(userDTO, existingUser);
+        UserEntity updatedUser = userRepository.save(existingUser);
+        return userMapper.toDTO(updatedUser);
     }
+//    public UserDTO updateUser(Integer id, UserDTO userDTO) {
+//
+//        Optional<UserEntity> existingUser = userRepository.findById(id);
+//        UserDTO updatedUser = null;
+//
+//        if(existingUser.isPresent()) {
+//            UserEntity userEntity = existingUser.get();
+//
+//            userEntity.setUsername(userDTO.getUsername());
+//            userEntity.setEmail(userDTO.getEmail());
+//            userEntity.setBirthDate(userDTO.getBirth());
+//            userEntity.setRole(userDTO.getRole());
+//            userEntity.setFirstname(userDTO.getFirstname());
+//            userEntity.setLastname(userDTO.getLastname());
+//
+//            UserEntity user = userRepository.save(userEntity);
+//            updatedUser = userMapper.toDTO(user);
+//        }
+//
+//        return updatedUser;
+//    }
 
 
     // DELETE
     public void deleteUser(Integer id) {
-        Optional<UserEntity> userEntity = userRepository.findById(id);
-
-        if(userEntity.isPresent()) {
-            userRepository.delete(userEntity.get());
-        }
+        UserEntity user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        userRepository.delete(user);
     }
-
-   /* public List<OrderDTO> getUserCheckoutHistory(Integer userId) {
-        List<TransactionEntity> transactions = transactionRepository.findByUserUserId(userId);
-        return transactions.stream()
-                .map(orderMapper::toDTO)
-                .collect(Collectors.toList());
-    }*/
 
 }
