91c507ea52af0e7242505c07c9d04f1120e834c9
matias <matiasalessandro1999@gmail.com>
2024-11-26 05:36:55 +0100

    conversion a string de las imagenes en base 64

--- src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/mappers/ProductMapper.java
+++ src/main/java/com/uade/tpo/g11/ecommerce/ecommerce/mappers/ProductMapper.java
@@ -20,49 +20,44 @@ public class ProductMapper {
     OrderDetailMapper orderDetailMapper;
 
     public ProductDTO toDTO(ProductEntity productEntity) {
-
-       ProductDTO productDTO = new ProductDTO();
-
+        ProductDTO productDTO = new ProductDTO();
         productDTO.setId(productEntity.getProductId());
         productDTO.setName(productEntity.getName());
         productDTO.setDescription(productEntity.getDescription());
-        productDTO.setImages(productEntity.getImages());
-        productDTO.setPrice(productEntity.getPrice());
+        productDTO.setImages(productEntity.getImages()); // Sin transformación
         productDTO.setPrice(productEntity.getPrice());
         productDTO.setStock(productEntity.getStock());
         productDTO.setCategory(productEntity.getCategory());
+        productDTO.setFeatured(productEntity.isFeatured());
 
         List<OrderDetailDTO> orderDetailsDTO = productEntity.getOrderDetails().stream()
                 .map(orderDetailMapper::toDTO)
                 .collect(Collectors.toList());
-
         productDTO.setOrderDetails(orderDetailsDTO);
-        productDTO.setFeatured(productEntity.isFeatured());
 
         return productDTO;
-
     }
+
 
     public ProductEntity toEntity(ProductDTO productDTO) {
+        ProductEntity productEntity = new ProductEntity();
+        productEntity.setProductId(productDTO.getId());
+        productEntity.setName(productDTO.getName());
+        productEntity.setDescription(productDTO.getDescription());
+        productEntity.setImages(productDTO.getImages()); // Sin transformación
+        productEntity.setPrice(productDTO.getPrice());
+        productEntity.setStock(productDTO.getStock());
+        productEntity.setCategory(productDTO.getCategory());
+        productEntity.setFeatured(productDTO.isFeatured());
 
-            ProductEntity productEntity = new ProductEntity();
+        List<OrderDetailEntity> orderDetailEntities = productDTO.getOrderDetails().stream()
+                .map(orderDetailMapper::toEntity)
+                .collect(Collectors.toList());
+        productEntity.setOrderDetails(orderDetailEntities);
 
-            productEntity.setProductId(productDTO.getId());
-            productEntity.setName(productDTO.getName());
-            productEntity.setDescription(productDTO.getDescription());
-            productEntity.setPrice(productDTO.getPrice());
-            productEntity.setCategory(productDTO.getCategory());
-            productEntity.setStock(productDTO.getStock());
-            productEntity.setImages(productDTO.getImages());
-
-            List<OrderDetailEntity> orderDetailEntities = productDTO.getOrderDetails().stream()
-                    .map(orderDetailMapper::toEntity)
-                    .collect(Collectors.toList());
-            productEntity.setOrderDetails(orderDetailEntities);
-            productEntity.setFeatured(productDTO.isFeatured());
-
-            return productEntity;
+        return productEntity;
     }
+
 
     public void updateEntityFromDTO(ProductDTO productDTO, ProductEntity productEntity) {
         productEntity.setProductId(productDTO.getId());
